572936c9f16909d17e2adde9f2b1cf44a091b396
Renad <Renad.Ali.ruh.k@tuwaiq.edu.sa>
2021-11-25 10:03:38 +0300

    image loading now

--- app/src/main/java/com/example/countryimages/BindingAdapters.kt
+++ app/src/main/java/com/example/countryimages/BindingAdapters.kt
@@ -1,23 +1,33 @@
 package com.example.countryimages
 
 import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import coil.ImageLoader
+import coil.decode.SvgDecoder
 import coil.load
+import coil.request.ImageRequest
 import com.example.countryimages.adapter.PhotoGridAdapter
 import com.example.countryimages.model.DataCountry
 
 
+
+
 @BindingAdapter("imageUrl")
-fun ImageView.bindImage( imageUrl: String?) {
-    imageUrl?.let {
-        val imgUri = imageUrl.toUri().buildUpon().scheme("https").build()
-        this.load(imgUri){
-            placeholder(R.drawable.loading_animation)
-            error(R.drawable.ic_broken_image)
-        }
-    }
+fun ImageView.loadSvg(imageUri: String?) {
+imageUri?.let{
+    val imageLoader = ImageLoader.Builder(this.context)
+        .componentRegistry { add(SvgDecoder(this@loadSvg.context)) }
+        .build()
+
+    this.load(uri = imageUri, imageLoader = imageLoader) {
+        crossfade(true)
+        placeholder(R.drawable.loading_animation)
+        error(R.drawable.ic_broken_image)    }
+}
+
 }
 
 @BindingAdapter("listData")
